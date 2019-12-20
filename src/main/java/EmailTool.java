e4923b9ec2bf4b1c44bf56b8ab757cac5669b4f7
bjxucan <bjxucan@corp.netease.com>
2019-12-20 18:30:35 +0800

    init

---
+++ src/main/java/EmailTool.java
@@ -0,0 +1,13 @@
+/**
+ *  Email tool (邮箱工具)
+ *  mainly include check a email address existed or not, address is legal format.
+ *  主要包括检测给定的邮箱地址是否真实存在，同时也可以检测邮箱地址格式是否合法.
+ * Author: chance hsu
+ * Date: 2019-12-20
+ */
+public class EmailTool {
+
+    public static void main(String[] args) {
+        System.out.println("hello world..");
+    }
+}
